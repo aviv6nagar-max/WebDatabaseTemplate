d704d3e90c534cd9a9d10937e801ba65244e6090
_ <_>
2026-04-29 11:35:31 +0300

    commit 33

--- Program.cs
+++ Program.cs
@@ -1,1 +1,87 @@
-﻿           
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Project.DatabaseUtilities;
+using Project.LoggingUtilities;
+using Project.ServerUtilities;
+
+class Program
+{
+    static void Main()
+    {
+        var server = new Server(5000);
+        var database = new Database();
+
+        Console.WriteLine("📚 Book Library Server Running");
+
+        while (true)
+        {
+            var request = server.WaitForRequest();
+
+            try
+            {
+               
+                if (request.Name == "getBooks")
+                {
+                    request.Respond(database.Books.ToList());
+                }
+
+                
+                else if (request.Name == "borrowBook")
+                {
+                    int id = request.GetParams<int>();
+
+                    var book = database.Books.FirstOrDefault(b => b.Id == id);
+
+                    if (book == null || book.stock <= 0)
+                    {
+                        request.Respond("Not available");
+                        continue;
+                    }
+
+                    book.stock--;
+                    database.SaveChanges();
+
+                    request.Respond("Borrowed");
+                }
+
+                
+                else if (request.Name == "buyBook")
+                {
+                    int id = request.GetParams<int>();
+
+                    var book = database.Books.FirstOrDefault(b => b.Id == id);
+
+                    if (book == null || book.stock <= 0)
+                    {
+                        request.Respond("Not available");
+                        continue;
+                    }
+
+                    book.stock--;
+                    database.SaveChanges();
+
+                    request.Respond("Bought");
+                }
+            }
+            catch (Exception ex)
+            {
+                request.SetStatusCode(500);
+                Log.WriteException(ex);
+            }
+        }
+    }
+}
+
+class Database() : DatabaseCore("database")
+{
+    public DbSet<Book> Books { get; set; } = default!;
+}
+
+class Book(string Title, int Author, int stock)
+{
+    public int Id { get; set; } = default!;
+    public string Title { get; set; } = Title;
+    public int Author { get; set; } = Author;
+    public int stock { get; set; } = stock;
+}
--- website/pages/index.ts
+++ website/pages/index.ts
@@ -1,23 +1,7 @@
-function borrowBook(id: number) {
-  fetch("http://localhost:5000/borrow", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ bookId: id })
-  })
-  .then(res => res.text())
-  .then(data => alert(data));
+function borrow(bookId: string): void {
+  console.log("Borrow book:", bookId);
 }
 
-function buyBook(id: number) {
-  fetch("http://localhost:5000/buy", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ bookId: id })
-  })
-  .then(res => res.text())
-  .then(data => alert(data));
+function buy(bookId: string): void {
+  console.log("Buy book:", bookId);
 }
