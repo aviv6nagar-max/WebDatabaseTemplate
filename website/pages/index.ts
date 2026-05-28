96aa9931cabf0d6e7494fcd94d8d333ace8cfa48
_ <_>
2026-05-28 14:25:59 +0300

    commit 55

--- website/pages/index.ts
+++ website/pages/index.ts
@@ -1,3 +1,14 @@
 function borrow(bookId: string): void {
   console.log("Borrow book:", bookId);
 }
+function goToBook(book: HTMLElement): void {
+    const bookId = book.getAttribute("data-id");
+    const bookName = book.getAttribute("data-name");
+
+    if (!bookId || !bookName) return;
+
+    window.location.href = `book.html?bookId=${bookId}&bookName=${encodeURIComponent(bookName)}`;
+}
+
+
+(window as any).goToBook = goToBook;
